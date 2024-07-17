1ce9b36b62bd013535acf727525f36dd66b9c0b8
Ivan Papko <ivanpapko@coherentsolutions.com>
2024-07-17 22:55:08 +0200

    Added the solution for the Task 31

---
+++ Appending-Variables-to-Strings.js
@@ -0,0 +1,8 @@
+//Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+
+//Solution
+
+const someAdjective = "funny";
+let myStr = "Learning to code is ";
+myStr+=someAdjective
