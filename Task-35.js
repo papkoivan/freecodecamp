128c64d24e76e1ed3402a9210965a71400ff6bd5
Ivan Papko <ivanpapko@coherentsolutions.com>
2024-07-17 23:31:37 +0200

    Added the solution for the Task 35

---
+++ Task-35.js
@@ -0,0 +1,8 @@
+// Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+
+// Solution
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2];
