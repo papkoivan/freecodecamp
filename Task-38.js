609a1f1891aad01c87e11644107dfbf563215477
Ivan Papko <ivanpapko@coherentsolutions.com>
2024-07-17 23:42:23 +0200

    Added the solution for the Task 38

---
+++ Task-38.js
@@ -0,0 +1,9 @@
+//In this challenge, we provide you with a noun, a verb, an adjective and an adverb.
+// You need to form a complete sentence using words of your choice, along with the words we provide.
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "My "+ myAdjective + " " + myNoun + " "+ myVerb + " really " + myAdverb;
