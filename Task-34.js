15cfc87dd22fd0d3985e3985c13ee4edcce42736
Ivan Papko <ivanpapko@coherentsolutions.com>
2024-07-17 23:09:48 +0200

    Added the solution for the Task 34

---
+++ Task-34.js
@@ -0,0 +1,7 @@
+//Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+
+// Solution
+// Setup
+let myStr = "Jello World";
+
+myStr = "Hello World"; 
