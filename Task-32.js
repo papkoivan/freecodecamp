dbcb75655fcf2c22c18b34a616245d232f2aa427
Ivan Papko <ivanpapko@coherentsolutions.com>
2024-07-17 22:58:41 +0200

    Added the solution for the Task 32

---
+++ Task-32.js
@@ -0,0 +1,8 @@
+//Use the .length property to set lastNameLength to the number of characters in lastName.
+
+// Solution
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
