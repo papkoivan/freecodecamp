81cfdcb3c65c03ba9947560eed5c42e1f000efb2
Ivan Papko <ivanpapko@coherentsolutions.com>
2024-07-17 23:04:36 +0200

    Added the solution for the Task 33

---
+++ Task-33.js
@@ -0,0 +1,11 @@
+// Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+
+// Solution
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
